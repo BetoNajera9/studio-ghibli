c49fec0612c689817d55a291d778939b7ebaba1d
BetoNajera <ing.betonajera@gmail.com>
2024-05-23 14:00:20 -0600

    :sparkles: Feat: Create auth guard
    Crete GAuth

---
+++ src/auth/guards/auth.guard.ts
@@ -0,0 +1,47 @@
+import { ConfigType } from '@nestjs/config'
+import { Request } from 'express'
+import * as Jwt from 'jsonwebtoken'
+import {
+	UnauthorizedException,
+	ExecutionContext,
+	CanActivate,
+	Injectable,
+	Inject,
+} from '@nestjs/common'
+
+import { config } from '../auth.config'
+import { EAuthResponse } from '@auth/enums'
+import { UserService } from '@user/user.service'
+import { IPayload } from '@auth/interfaces'
+
+@Injectable()
+export class GAuth implements CanActivate {
+	constructor(
+		@Inject(config.KEY)
+		private readonly configService: ConfigType<typeof config>,
+		private readonly userService: UserService
+	) {}
+
+	async canActivate(context: ExecutionContext): Promise<boolean> {
+		const req = context.switchToHttp().getRequest<Request>()
+
+		const bearerToken = req.headers['authorization']
+		if (!bearerToken) throw new UnauthorizedException('Invalid Token')
+
+		const token = bearerToken.replace('Bearer ', '')
+
+		const manageToken = Jwt.verify(
+			token,
+			this.configService.jwtSecret
+		) as IPayload
+
+		if (!manageToken) throw new UnauthorizedException('Invalid Token')
+
+		const user = await this.userService.findOne(manageToken.sub)
+		if (!user) throw new UnauthorizedException('Invalid Token')
+
+		req.params.user = manageToken.sub
+
+		return true
+	}
+}
